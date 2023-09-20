6a600dc0d6ba2fb09f05bf804ac11c930a67e258
Sahil Mund <130903382+sahil-009@users.noreply.github.com>
2023-09-20 19:05:33 +0530

    Add files via upload

---
+++ QR-Generator2.py
@@ -0,0 +1,12 @@
+import qrcode
+from PIL import Image
+
+qr=qrcode.QRCode(version=1,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H,
+                 box_size=10,border=4,)
+qr.add_data("https://github.com/sahil-009")
+qr.make(fit=True)
+img=qr.make_image(fill_color="red",back_color="blue")
+img.save("GitHub Profile color.png")
+
+
