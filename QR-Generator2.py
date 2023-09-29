4df3bbae678a548deaff6063e9c17a385ef74ec5
Sahil Mund <130903382+sahil-009@users.noreply.github.com>
2023-09-29 18:24:07 +0530

    Update QR-Generator2.py

--- QR-Generator2.py
+++ QR-Generator2.py
@@ -6,7 +6,7 @@ qr=qrcode.QRCode(version=1,
                  box_size=10,border=4,)
 qr.add_data("https://github.com/sahil-009")
 qr.make(fit=True)
-img=qr.make_image(fill_color="red",back_color="blue")
+img=qr.make_image(fill_color="white",back_color="slateblue")
 img.save("GitHub Profile color.png")
 
 
